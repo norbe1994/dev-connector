b3cff3a87b7b01a8c38cf093bec2925ea8150afc
Norberto <caceresnorbertoenrique@gmail.com>
2019-05-17 13:54:42 -0500

    register non-existing user

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const router = express.Router()
+const gravatar = require('gravatar')
+const bcrypt = require('bcryptjs')
 const { validationResult } = require('express-validator/check')
+
+const User = require('../../models/User')
 
 // Validations
 const userValidations = require('../../validations/User')
@@ -8,11 +12,44 @@ const userValidations = require('../../validations/User')
 // @route GET api/users
 // @desc Register user
 // @access Public
-router.post('/', userValidations, (req, res) => {
+router.post('/', userValidations, async (req, res) => {
 	const errors = validationResult(req)
 
 	if (!errors.isEmpty()) {
 		return res.status(400).json({ errors: errors.array() })
+	}
+
+	const { name, email, password } = req.body
+
+	try {
+		// see if user exists
+		let user = await User.findOne({ email })
+		if (user) {
+			return res.status(400).json({ errors: [{ msg: 'User already exists' }] })
+		}
+		// get user's grvatar
+		const avatar = gravatar.url(email, {
+			s: '200',
+			r: 'pg',
+			d: 'mm',
+		})
+		// create user and encrypt password
+		user = new User({
+			name,
+			email,
+			avatar,
+			password,
+		})
+
+		const salt = await bcrypt.genSalt(10)
+		user.password = await bcrypt.hash(password, salt)
+		await user.save()
+
+		// return jswonwebtoken
+		res.send('User registered')
+	} catch (err) {
+		console.log(err)
+		res.status(500).send('Server error')
 	}
 
 	res.send('User route')
