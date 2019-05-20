1e5c3a7b48d715cdc1aa0c7945da8f05efc6712f
Norberto <caceresnorbertoenrique@gmail.com>
2019-05-20 10:26:12 -0500

    create delete profile and user endpoint

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -137,4 +137,23 @@ router.get('/user/:user_id', async (req, res) => {
 	}
 })
 
+// @route DELETE api/profile
+// @desc Delete profile, user & posts
+// @access Private
+router.delete('/', auth, async (req, res) => {
+	try {
+		// @todo - remove user's posts
+
+		// remove profile
+		await Profile.findOneAndRemove({ user: req.user.id })
+		// remove user
+		await User.findOneAndRemove({ _id: req.user.id })
+
+		res.json({ msg: 'User deleted' })
+	} catch (error) {
+		console.error(error.message)
+		res.status(500).send('Server error')
+	}
+})
+
 module.exports = router
