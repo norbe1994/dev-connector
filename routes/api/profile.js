2df0a7920f1fb292f29d53d5e12e79d9c2358bb0
Norberto <caceresnorbertoenrique@gmail.com>
2019-05-20 10:14:53 -0500

    create route to get specific profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -102,4 +102,39 @@ router.post('/', [auth, profileCreationValidations], async (req, res) => {
 	}
 })
 
+// @route GET api/profile
+// @desc Get all profiles
+// @access Public
+router.get('/', async (req, res) => {
+	try {
+		const profiles = await Profile.find().populate('user', ['name', 'avatar'])
+		res.json(profiles)
+	} catch (error) {
+		console.error(error.message)
+		res.status(500).send('Server error')
+	}
+})
+
+// @route GET api/profile/user/:user_id
+// @desc Get profile by user ID
+// @access Public
+router.get('/user/:user_id', async (req, res) => {
+	try {
+		const profile = await Profile.findOne({
+			user: req.params.user_id,
+		}).populate('user', ['name', 'avatar'])
+
+		if (!profile)
+			return res.status(400).json({ msg: 'There is no profile for this user' })
+
+		res.json(profile)
+	} catch (error) {
+		console.error(error.message)
+		if (error.kind == 'ObjectId') {
+			return res.status(400).json({ msg: 'There is no profile for this user' })
+		}
+		res.status(500).send('Server error')
+	}
+})
+
 module.exports = router
