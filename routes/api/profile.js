e5b7c19d8352c6104b9b990329ad21901f70b1c9
Norberto <caceresnorbertoenrique@gmail.com>
2019-05-20 11:18:48 -0500

    create endpoint to create and delete educations to profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const auth = require('../../middleware/auth')
 const { validationResult } = require('express-validator/check')
+const ObjectId = require('mongoose').Types.ObjectId
 
 // models
 const User = require('../../models/User')
@@ -194,7 +195,9 @@ router.put(
 
 		try {
 			const profile = await Profile.findOne({ user: req.user.id })
+
 			profile.experience.unshift(newExperience)
+
 			await profile.save()
 
 			res.json(profile)
@@ -204,5 +207,97 @@ router.put(
 		}
 	}
 )
+
+// @route DELETE api/profile/experience/:exp_id
+// @desc Remove experience from profile
+// @access Private
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+	try {
+		// the new option is added so mongoose return the modified object and not the original
+		const profile = await Profile.findOneAndUpdate(
+			{ user: req.user.id },
+			{ $pull: { experience: { _id: new ObjectId(req.params.exp_id) } } },
+			{ new: true }
+		)
+		await profile.save()
+		res.status(200).json(profile)
+	} catch (error) {
+		console.error(error.message)
+		if (error.kind == 'ObjectId') {
+			return res.status(400).json({ msg: 'Incorrect ID format' })
+		}
+		res.status(500).send('Server error')
+	}
+})
+
+// @route PUT api/profile/education
+// @desc Add education to profile
+// @access Private
+router.put(
+	'/education',
+	[auth, profileValidations.educationCreation],
+	async (req, res) => {
+		const errors = validationResult(req)
+
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() })
+		}
+
+		const {
+			school,
+			degree,
+			fieldofstudy,
+			from,
+			to,
+			current,
+			description,
+		} = req.body
+
+		const newEdu = {
+			school,
+			degree,
+			fieldofstudy,
+			from,
+			to,
+			current,
+			description,
+		}
+
+		try {
+			const profile = await Profile.findOne({ user: req.user.id })
+
+			profile.education.unshift(newEdu)
+
+			await profile.save()
+
+			res.json(profile)
+		} catch (error) {
+			console.error(error.message)
+			res.status(500).send('Server error')
+		}
+	}
+)
+
+// @route DELETE api/profile/education/:edu_id
+// @desc Remove education from profile
+// @access Private
+router.delete('/education/:edu_id', auth, async (req, res) => {
+	try {
+		// the new option is added so mongoose return the modified object and not the original
+		const profile = await Profile.findOneAndUpdate(
+			{ user: req.user.id },
+			{ $pull: { education: { _id: new ObjectId(req.params.edu_id) } } },
+			{ new: true }
+		)
+		await profile.save()
+		res.status(200).json(profile)
+	} catch (error) {
+		console.error(error.message)
+		if (error.kind == 'ObjectId') {
+			return res.status(400).json({ msg: 'Incorrect ID format' })
+		}
+		res.status(500).send('Server error')
+	}
+})
 
 module.exports = router
--- validations/Profile.js
+++ validations/Profile.js
@@ -20,4 +20,18 @@ module.exports = validations = {
 			.not()
 			.isEmpty(),
 	],
+	educationCreation: [
+		check('school', 'School is required')
+			.not()
+			.isEmpty(),
+		check('degree', 'Degree is required')
+			.not()
+			.isEmpty(),
+		check('fieldofstudy', 'Field of study is required')
+			.not()
+			.isEmpty(),
+		check('from', 'From date is required')
+			.not()
+			.isEmpty(),
+	],
 }
